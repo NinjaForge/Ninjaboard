8a6fd6e5926e9d9591383f2a372434d715f96fd2
Stian Didriksen <stipsan@me.com>
2011-12-21 22:57:24 +0100

    importing from svn

---
+++ mod_ninjaboard_posts/mod_ninjaboard_posts.php
@@ -0,0 +1,41 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+/**
+ * @version		$Id: mod_ninjaboard_posts.php 2278 2011-07-24 23:11:56Z captainhook $
+ * @category	Ninjaboard
+ * @copyright	Copyright (C) 2007 - 2011 NinjaForge. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link     	http://ninjaforge.com
+ */
+
+/** 
+* If koowa is unavailable
+* Abort the dispatcher
+*/
+if( !defined('KOOWA') ) return;
+
+/** 
+* If Ninjaboard is unavailable
+* Abort the dispatcher
+*/
+if( !KLoader::path('site::com.ninjaboard.dispatcher') ) return;
+
+//Initialize the dispatcher just so models are mapped, and everything else Ninjaboard needs to run
+KLoader::load('site::com.ninjaboard.dispatcher');
+ComNinjaboardDispatcher::register();
+
+/**
+ * Renders latest posts with the same look and feel like the regular component list
+ *
+ * @version		$Id: mod_ninjaboard_posts.php 2278 2011-07-24 23:11:56Z captainhook $
+ * @category	Ninjaboard
+ * @copyright	Copyright (C) 2007 - 2011 NinjaForge. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link     	http://ninjaforge.com
+ */
+
+
+echo KFactory::tmp('site::mod.ninjaboard_posts.html', array(
+	'params'  => $params,
+	'module'  => $module,
+	'attribs' => $attribs
+))->display();
