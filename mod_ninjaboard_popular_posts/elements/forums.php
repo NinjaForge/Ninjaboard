8354a68a5b7b69f1b5fc3c47fee957adec087d2d
Stian Didriksen <stipsan@me.com>
2011-12-21 22:57:12 +0100

    importing from svn

---
+++ mod_ninjaboard_popular_posts/elements/forums.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* @version		1.0.7
+* @package		mod_ninjaboard_popular_posts
+* @author 		NinjaForge
+* @author email	support@ninjaforge.com
+* @link			http://ninjaforge.com
+* @license      http://www.gnu.org/copyleft/gpl.html GNU GPL
+* @copyright	Copyright (C) 2010 NinjaForge - All rights reserved.
+*/
+
+class JElementForums extends JElement
+
+{
+
+   var   $_name = 'Forums';
+
+   function fetchElement($name, $value, &$node, $control_name)
+
+   {
+
+		if (file_exists(JPATH_SITE.'/components/com_ninjaboard/ninjaboard.php')){
+
+			$size = ( $node->attributes('size') ? $node->attributes('size') : 5 );
+			
+			$db =& JFactory::getDBO();
+			
+			$query = 'SELECT ninjaboard_forum_id AS id, title FROM #__ninjaboard_forums WHERE enabled = 1 ORDER BY title';
+			
+			$db->setQuery($query);
+			
+			$options = $db->loadObjectList();
+			
+			return JHTML::_('select.genericlist',  $options, ''.$control_name.'['.$name.'][]',  ' multiple="multiple" size="' . $size .'" class="inputbox"', 'id', 'title', $value, $control_name.$name);
+
+	   } else {
+
+		   return JText::_('Ninjaboard is not Installed!');
+
+	   }
+
+   }
+
+}
+
