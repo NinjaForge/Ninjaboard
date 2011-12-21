e0a5ab459d3d7885b067159a7be89109e5d9b529
Stian Didriksen <stipsan@me.com>
2011-12-21 18:16:46 +0100

    importing from svn

---
+++ plg_authentication_smf/smf.php
@@ -0,0 +1,78 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+/**
+ * @version		$Id: smf.php 1720 2011-03-28 02:39:44Z stian $
+ * @category	Ninjaboard
+ * @copyright	Copyright (C) 2007 - 2011 NinjaForge. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link     	http://ninjaforge.com
+ */
+
+jimport( 'joomla.plugin.plugin' );
+
+/**
+ * SMF authentication plugin
+ *
+ * This is a companion plugin to the Ninjaboard SMF converter, which allows stand-alone SMF imports to let users to login with their old passwords
+ * 
+ * @author Stian Didriksen <stian@ninjaforge.com>
+ */
+class plgAuthenticationSmf extends JPlugin
+{
+	/**
+	 * This method should handle any authentication and report back to the subject
+	 *
+	 * @access	public
+	 * @param   array 	$credentials Array holding the user credentials
+	 * @param 	array   $options     Array of extra options
+	 * @param	object	$response	 Authentication response object
+	 * @return	boolean
+	 * @since 1.5
+	 */
+	function onAuthenticate( $credentials, $options, &$response )
+	{
+		jimport('joomla.user.helper');
+
+		// Joomla does not like blank passwords
+		if (empty($credentials['password']))
+		{
+			$response->status = JAUTHENTICATE_STATUS_FAILURE;
+			$response->error_message = 'Empty password not allowed';
+			return false;
+		}
+
+		// Initialize variables
+		$conditions = '';
+
+		// Get a database object
+		$db =& JFactory::getDBO();
+
+		$query = 'SELECT `id`, `password`, `gid`'
+			. ' FROM `#__users`'
+			. ' WHERE username=' . $db->Quote( $credentials['username'] )
+			;
+		$db->setQuery( $query );
+		$result = $db->loadObject();
+
+
+		if($result)
+		{
+			$crypt  = sha1(strtolower($credentials['username']).$credentials['password']);
+
+			if ($crypt == $result->password) {
+				$user = JUser::getInstance($result->id); // Bring this in line with the rest of the system
+				$response->email = $user->email;
+				$response->fullname = $user->name;
+				$response->status = JAUTHENTICATE_STATUS_SUCCESS;
+				$response->error_message = '';
+			} else {
+				$response->status = JAUTHENTICATE_STATUS_FAILURE;
+				$response->error_message = 'Invalid password';
+			}
+		}
+		else
+		{
+			$response->status = JAUTHENTICATE_STATUS_FAILURE;
+			$response->error_message = 'User does not exist';
+		}
+	}
+}
