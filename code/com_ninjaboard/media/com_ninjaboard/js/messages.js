e9647575b5efbebd2ad7ca5cfa087b9b1f075cd0
Stian Didriksen <stipsan@me.com>
2012-02-15 18:21:53 +0100

    jQuery to ninja

--- code/com_ninjaboard/media/com_ninjaboard/js/messages.js
+++ code/com_ninjaboard/media/com_ninjaboard/js/messages.js
@@ -1,4 +1,4 @@
-jQuery(function($){
+ninja(function($){
 	$('.message-header-disclosure-triangle').live('click', function(){
 		$(this).closest('.splitview').toggleClass('message-header-expand');
 	});
