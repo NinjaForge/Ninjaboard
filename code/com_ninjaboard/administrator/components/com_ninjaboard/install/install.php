e4ecf5946231d5786ec13ce1358eba3eccbf499d
Richie Mortimer <richie@ninjaforge.com>
2012-07-17 16:03:08 +0100

    removing com_koowa should be on the upgrade metho

--- code/com_ninjaboard/administrator/components/com_ninjaboard/install/install.php
+++ code/com_ninjaboard/administrator/components/com_ninjaboard/install/install.php
@@ -13,3 +13,17 @@ JLoader::register('JDependent', dirname(__FILE__).'/jdependent/jdependent.php');
 $installer = new JDependent();
 
 $installer->install('nooku')->install('ninja');
+
+// if com_koowa exists remove it
+if (JFolder::exists(JPATH_ADMINISTRATOR.'/components/com_koowa')) {
+	JFolder::delete(JPATH_ADMINISTRATOR.'/components/com_koowa');
+	
+	if (JFolder::exists(JPATH_ROOT.'/media/com_koowa')) {
+		JFolder::delete(JPATH_ROOT.'/media/com_koowa');
+	}
+
+	// remove it from the database
+	$db = JFactory::getDBO();
+	$db->setQuery("DELETE FROM `#__components` WHERE `option` = 'com_koowa' OR `option` = 'com_ninja';");
+	$db->execute();
+}
--- code/com_ninjaboard/administrator/components/com_ninjaboard/install/script.php
+++ code/com_ninjaboard/administrator/components/com_ninjaboard/install/script.php
@@ -7,7 +7,7 @@ JLoader::register('JDependent', dirname(__FILE__).'/jdependent/jdependent.php');
 /**
  * Installation Script Files
  */
-class com_ninjaboardInstallerScript
+class com_NinjaboardInstallerScript
 {
 	/**
 	 * method to install the component
@@ -19,19 +19,6 @@ class com_ninjaboardInstallerScript
 		$installer = new JDependent();
 
 		$installer->install('nooku')->install('ninja');
-		error_log('does it exist? '.JFolder::exists(JPATH_ADMINISTRATOR.'/components/com_koowa'));
-		// if com_koowa exists remove it
-		if (JFolder::exists(JPATH_ADMINISTRATOR.'/components/com_koowa')) {
-			error_log('it exists');
-			JFolder::delete(JPATH_ADMINISTRATOR.'/components/com_koowa');
-			JFolder::delete(JPATH_ROOT.'/media/com_koowa');
-
-			// remove it from the database
-			$db = JFactory::getDBO();
-			$db->setQuery("DELETE FROM `#__extensions` WHERE `element` = 'com_koowa' OR `element` = 'com_ninja';");
-			error_log("DELETE FROM `#__extensions` WHERE `element` = 'com_koowa' OR `element` = 'com_ninja';");
-			$db->execute();
-		}
 	}
  
 	/**
@@ -44,5 +31,31 @@ class com_ninjaboardInstallerScript
 		$installer = new JDependent();
 
 		$installer->uninstall('nooku')->uninstall('ninja');
+	}
+
+	/**
+	 * method to upgrade the component
+	 *
+	 * @return void
+	 */
+	function update($parent)
+	{
+		$installer = new JDependent();
+
+		$installer->install('nooku')->install('ninja');
+
+		// if com_koowa exists remove it
+		if (JFolder::exists(JPATH_ADMINISTRATOR.'/components/com_koowa')) {
+			JFolder::delete(JPATH_ADMINISTRATOR.'/components/com_koowa');
+			
+			if (JFolder::exists(JPATH_ROOT.'/media/com_koowa')) {
+				JFolder::delete(JPATH_ROOT.'/media/com_koowa');
+			}
+
+			// remove it from the database
+			$db = JFactory::getDBO();
+			$db->setQuery("DELETE FROM `#__extensions` WHERE `element` = 'com_koowa' OR `element` = 'com_ninja';");
+			$db->execute();
+		}
 	}
 }
